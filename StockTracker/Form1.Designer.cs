24b81165fcbe2ece5ed88b41b3bc5136074632f6
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-04-20 07:26:48 -0700

    Added UI elements

--- StockTracker/Form1.Designer.cs
+++ StockTracker/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._listViewStocks = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this._textBoxTicker = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // _listViewStocks
+            // 
+            this._listViewStocks.Location = new System.Drawing.Point(12, 138);
+            this._listViewStocks.Name = "_listViewStocks";
+            this._listViewStocks.Size = new System.Drawing.Size(761, 288);
+            this._listViewStocks.TabIndex = 0;
+            this._listViewStocks.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stocks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Ticker:";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(798, 138);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 3;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // _textBoxTicker
+            // 
+            this._textBoxTicker.Location = new System.Drawing.Point(107, 22);
+            this._textBoxTicker.Name = "_textBoxTicker";
+            this._textBoxTicker.Size = new System.Drawing.Size(100, 22);
+            this._textBoxTicker.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1075, 451);
+            this.Controls.Add(this._textBoxTicker);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._listViewStocks);
+            this.Name = "Form1";
+            this.Text = "Stock Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView _listViewStocks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox _textBoxTicker;
     }
 }
 
