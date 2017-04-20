3b89aa6255bab816017266d622cced5767d208ba
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-04-20 08:14:00 -0700

    UI

--- StockTracker/Form1.Designer.cs
+++ StockTracker/Form1.Designer.cs
@@ -29,26 +29,41 @@
         private void InitializeComponent()
         {
             this._listViewStocks = new System.Windows.Forms.ListView();
+            this.Ticker = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quote = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonRefresh = new System.Windows.Forms.Button();
             this._textBoxTicker = new System.Windows.Forms.TextBox();
-            this.Ticker = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Quote = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this._buttonAddTicker = new System.Windows.Forms.Button();
+            this._textBoxShares = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Shares = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // _listViewStocks
             // 
             this._listViewStocks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.Ticker,
-            this.Quote});
+            this.Quote,
+            this.Shares,
+            this.Value});
             this._listViewStocks.Location = new System.Drawing.Point(12, 138);
             this._listViewStocks.Name = "_listViewStocks";
             this._listViewStocks.Size = new System.Drawing.Size(761, 288);
             this._listViewStocks.TabIndex = 0;
             this._listViewStocks.UseCompatibleStateImageBehavior = false;
             this._listViewStocks.View = System.Windows.Forms.View.Details;
+            // 
+            // Ticker
+            // 
+            this.Ticker.Text = "Ticker";
+            this.Ticker.Width = 120;
+            // 
+            // Quote
+            // 
+            this.Quote.Text = "Quote";
             // 
             // label1
             // 
@@ -86,18 +101,9 @@
             this._textBoxTicker.TabIndex = 4;
             this._textBoxTicker.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TickerKeyPress);
             // 
-            // Ticker
-            // 
-            this.Ticker.Text = "Ticker";
-            this.Ticker.Width = 120;
-            // 
-            // Quote
-            // 
-            this.Quote.Text = "Quote";
-            // 
             // _buttonAddTicker
             // 
-            this._buttonAddTicker.Location = new System.Drawing.Point(242, 22);
+            this._buttonAddTicker.Location = new System.Drawing.Point(241, 53);
             this._buttonAddTicker.Name = "_buttonAddTicker";
             this._buttonAddTicker.Size = new System.Drawing.Size(75, 23);
             this._buttonAddTicker.TabIndex = 5;
@@ -105,11 +111,37 @@
             this._buttonAddTicker.UseVisualStyleBackColor = true;
             this._buttonAddTicker.Click += new System.EventHandler(this.AddTicker);
             // 
+            // _textBoxShares
+            // 
+            this._textBoxShares.Location = new System.Drawing.Point(107, 56);
+            this._textBoxShares.Name = "_textBoxShares";
+            this._textBoxShares.Size = new System.Drawing.Size(100, 22);
+            this._textBoxShares.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter Shares:";
+            // 
+            // Shares
+            // 
+            this.Shares.Text = "Shares";
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1075, 451);
+            this.Controls.Add(this._textBoxShares);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this._buttonAddTicker);
             this.Controls.Add(this._textBoxTicker);
             this.Controls.Add(this.buttonRefresh);
@@ -133,6 +165,10 @@
         private System.Windows.Forms.ColumnHeader Ticker;
         private System.Windows.Forms.ColumnHeader Quote;
         private System.Windows.Forms.Button _buttonAddTicker;
+        private System.Windows.Forms.ColumnHeader Shares;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.TextBox _textBoxShares;
+        private System.Windows.Forms.Label label3;
     }
 }
 
