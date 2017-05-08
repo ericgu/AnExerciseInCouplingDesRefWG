8cc26c230c22618237a22c0b33e41df7e707e242
ericgu <ericgu@microsoft.com>
2017-05-08 16:08:06 -0700

    R: move to namespace

--- StockTracker/StockPriceLoader.cs
+++ StockTracker/StockPriceLoader.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Net;
 
-internal class StockPriceLoader
+namespace StockTracker
 {
-    public double Load(string url)
+    internal class StockPriceLoader
     {
-        double price = 0;
-        WebClient webClient = new WebClient();
-        string result = webClient.DownloadString(url);
-        if (!result.Contains("No symbol matches found"))
+        public double Load(string url)
         {
-            string lastPrice = result.Substring(result.IndexOf("LastPrice"));
-            string pricePlus = lastPrice.Substring(lastPrice.IndexOf(":") + 1);
-            string priceString = pricePlus.Substring(0, pricePlus.IndexOf(","));
-            price = Double.Parse(priceString);
+            double price = 0;
+            WebClient webClient = new WebClient();
+            string result = webClient.DownloadString(url);
+            if (!result.Contains("No symbol matches found"))
+            {
+                string lastPrice = result.Substring(result.IndexOf("LastPrice"));
+                string pricePlus = lastPrice.Substring(lastPrice.IndexOf(":") + 1);
+                string priceString = pricePlus.Substring(0, pricePlus.IndexOf(","));
+                price = Double.Parse(priceString);
+            }
+            return price;
         }
-        return price;
     }
 }
