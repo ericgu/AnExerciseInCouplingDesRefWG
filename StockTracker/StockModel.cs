03e7a3dda35d3ecd167ad43c295a98d4582cda6b
valenti <valenti@VALENTI-LAPTOP>
2017-05-09 17:58:45 -0700

    Added Changed event to StockModel

--- StockTracker/StockModel.cs
+++ StockTracker/StockModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace StockTracker
@@ -5,6 +6,7 @@ namespace StockTracker
     public class StockModel
     {
         private readonly List<Stock> _stocks;
+        public event EventHandler Changed;
 
         public StockModel(List<Stock> stocks)
         {
@@ -14,16 +16,23 @@ namespace StockTracker
         public void Add(string ticker, double shares, double purchasePrice, string purchaseDate)
         {
             this._stocks.Add(new Stock(ticker, shares, purchasePrice, purchaseDate));
+            OnChanged();
         }
 
         public void RemoveAt(int index)
         {
             this._stocks.RemoveAt(index);
+            OnChanged();
         }
 
         public IEnumerable<Stock> EnumerateStocks()
         {
             return this._stocks;
+        }
+
+        private void OnChanged()
+        {
+            Changed?.Invoke(this, EventArgs.Empty);
         }
     }
 }
