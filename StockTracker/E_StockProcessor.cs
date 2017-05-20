94d93a45b933ce6aaa535031b64ddfba6e5c14cf
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-05-20 08:14:36 -0700

    Extract list operaratinos to methods

--- StockTracker/E_StockProcessor.cs
+++ StockTracker/E_StockProcessor.cs
@@ -7,35 +7,36 @@ namespace StockTracker
     {
         public static void RefreshTable(StockCollection stockCollection, GainModel gainModel, ListView listViewStocks)
         {
-            listViewStocks.Items.Clear();
+            ClearList(listViewStocks);
 
             var stockPriceStockTotalPriceStockGains = gainModel.GetModel(stockCollection.EnumerateStocks()).ToList();
             foreach (var s in stockPriceStockTotalPriceStockGains)
             {
                 var stock = s.Stock;
-                var listViewItem = CreateListViewItem(stock.Ticker, s.Price, stock.Shares, s.StockTotalPrice,
-                    s.StockGain);
-                listViewStocks.Items.Add(listViewItem);
+                AddItemToList(listViewStocks, stock.Ticker, s.Price, stock.Shares, s.StockTotalPrice, s.StockGain);
             }
 
-            var listViewItemLine = CreateListViewItem("------", "-", "-", "-");
-            listViewStocks.Items.Add(listViewItemLine);
+            AddItemToList(listViewStocks, "------", "-", "-", "-");
 
-            var listViewItemTotal = CreateListViewItem("Total", "-", "-",
+            AddItemToList(listViewStocks, "Total", "-", "-",
                 stockPriceStockTotalPriceStockGains.Sum(s => s.StockTotalPrice),
                 stockPriceStockTotalPriceStockGains.Sum(s => s.StockGain));
-            listViewStocks.Items.Add(listViewItemTotal);
         }
 
-        public static ListViewItem CreateListViewItem(params object[] parameters)
+        private static void AddItemToList(ListView listViewStocks, params object[] parameters)
         {
             var listViewItem = new ListViewItem(parameters[0].ToString());
 
-            for (int i = 1; i < parameters.Length; i++)
+            for (int i = 1; i < new[] {parameters}.Length; i++)
             {
-                listViewItem.SubItems.Add(parameters[i].ToString());
+                listViewItem.SubItems.Add(new[] {parameters}[i].ToString());
             }
-            return listViewItem;
+            listViewStocks.Items.Add(listViewItem);
+        }
+
+        private static void ClearList(ListView listViewStocks)
+        {
+            listViewStocks.Items.Clear();
         }
     }
 }
