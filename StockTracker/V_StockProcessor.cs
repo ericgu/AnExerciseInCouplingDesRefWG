6defcd67c3dccd9547296337ce63dc276eb56ea2
valenti <valenti@VALENTI-LAPTOP>
2017-05-21 18:15:22 -0700

    R: Extracted Foo class from list view subitems

--- StockTracker/V_StockProcessor.cs
+++ StockTracker/V_StockProcessor.cs
@@ -3,6 +3,16 @@ using System.Windows.Forms;
 
 namespace StockTracker
 {
+    internal class Foo
+    {
+        public Foo(params object[] parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public object[] Parameters { get; }
+    }
+
     internal static class V_StockProcessor
     {
         public static void RefreshTable(StockCollection stockCollection, GainModel gainModel, ListView listViewStocks)
@@ -13,27 +23,24 @@ namespace StockTracker
             foreach (var s in stockPriceStockTotalPriceStockGains)
             {
                 var stock = s.Stock;
-                var listViewItem = CreateListViewItem(stock.Ticker, s.Price, stock.Shares, s.StockTotalPrice,
-                    s.StockGain);
+                var listViewItem = CreateListViewItem(new Foo(stock.Ticker, s.Price, stock.Shares, s.StockTotalPrice, s.StockGain));
                 listViewStocks.Items.Add(listViewItem);
             }
 
-            var listViewItemLine = CreateListViewItem("------", "-", "-", "-");
+            var listViewItemLine = CreateListViewItem(new Foo("------", "-", "-", "-"));
             listViewStocks.Items.Add(listViewItemLine);
 
-            var listViewItemTotal = CreateListViewItem("Total", "-", "-",
-                stockPriceStockTotalPriceStockGains.Sum(s => s.StockTotalPrice),
-                stockPriceStockTotalPriceStockGains.Sum(s => s.StockGain));
+            var listViewItemTotal = CreateListViewItem(new Foo("Total", "-", "-", stockPriceStockTotalPriceStockGains.Sum(s => s.StockTotalPrice), stockPriceStockTotalPriceStockGains.Sum(s => s.StockGain)));
             listViewStocks.Items.Add(listViewItemTotal);
         }
 
-        public static ListViewItem CreateListViewItem(params object[] parameters)
+        public static ListViewItem CreateListViewItem(Foo foo)
         {
-            var listViewItem = new ListViewItem(parameters[0].ToString());
+            var listViewItem = new ListViewItem(foo.Parameters[0].ToString());
 
-            for (int i = 1; i < parameters.Length; i++)
+            for (int i = 1; i < foo.Parameters.Length; i++)
             {
-                listViewItem.SubItems.Add(parameters[i].ToString());
+                listViewItem.SubItems.Add(foo.Parameters[i].ToString());
             }
             return listViewItem;
         }
