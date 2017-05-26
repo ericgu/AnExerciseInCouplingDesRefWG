663e7259e23780f9a20ebc93013f2c061e5818fd
ericgu <ericgu@microsoft.com>
2017-05-26 13:06:46 -0700

    Added stock store interface

---
+++ StockTracker/E_IStocksStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StockTracker
+{
+    public interface E_IStocksStore
+    {
+        void SaveStocks(IEnumerable<Stock> stocks);
+        List<Stock> LoadStocks();
+    }
+}
